d8442048f37ff873d3f5146523908d471c026015
Pagunasa <guillem.martinez1996@gmail.com>
2021-10-18 18:18:55 +0200

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,6 +1,19 @@
 // create a new scene
 let gameScene = new Phaser.Scene('Game');
 
+// load assets
+gameScene.preload = function() {
+    // load images
+    this.load.image('background', 'assets/background.png')
+    this.load.image('player', 'assets/player.png')
+}
+
+// called once after the preload ends
+gameScene.create = function() {
+    // create background sprite
+    this.add.sprite(0,0, 'background');
+}
+
 // set config of the game
 let config = {
     type: Phaser.AUTO,
