0a1f9681c4fe47bdb18c88748b633d7baad62f87
Pagunasa <guillem.martinez1996@gmail.com>
2021-10-19 16:29:56 +0200

    Tower grid creation

--- js/game.js
+++ js/game.js
@@ -1,29 +1,58 @@
 // create a new scene
 let gameScene = new Phaser.Scene('Game');
 
+let levels = { sizeX: 10, sizeY: 10, tileSizeX: 32, tileSizeY: 32 };
+let tileSize = [32, 32];
+
 // load assets
 gameScene.preload = function() {
     // load images
-    this.load.image('background', 'assets/background.png')
-    this.load.image('player', 'assets/player.png')
-    this.load.image('dragon', 'assets/dragon.png')
+    this.load.image('atlas', 'assets/atlas.png');
+}
+
+function createMapData(level) {
+    let data = [], tiles;
+
+    for(let i = 0; i < level.sizeX; ++i) {
+        
+        tiles = [];
+
+        for(let j = 0; j < level.sizeY; ++j)
+            tiles[j] = Phaser.Math.Between(0, 1);
+        
+        data[i] = tiles;
+    }
+
+    return data;
 }
 
 // called once after the preload ends
 gameScene.create = function() {
-    // create background sprite
-    let bg = this.add.sprite(0,0, 'background');
+    // create the level 
+    let dataMap =  createMapData(levels);
     
-    // change the origin
-    bg.setOrigin(0,0);
+    // prepare the tilemap (atlas)
+    let map = this.make.tilemap({ key: 'map', tileWidth: levels.tileSizeX, tileHeight: levels.tileSizeY });
+    let tileset = map.addTilesetImage('MapAtlas', 'atlas', levels.tileSizeX, levels.tileSizeY); 
+    
+    this.groundLayer = map.createBlankLayer('Ground', tileset);
 
-    // adding the player
-    let player = this.add.sprite(70, 180, 'player');
-    player.setScale(0.5, 0.5);
+    let data;
 
-    let dragon = this.add.sprite(250, 180, 'dragon');
-    dragon.flipX = true;
+    for(let i = 0; i < dataMap.length; ++i) {
+        data = dataMap[i];
+        for(let j = 0; j < data.length; ++j) 
+            this.groundLayer.putTileAt(data[j], level.tileSizeX * j, level.tileSizeY * i);
+    }
+
+    //var sprites = map.createFromTiles(indexes, replacements, spriteConfig);
 }
+
+
+gameScene.update = function() {
+    
+}
+
 
 // set config of the game
 let config = {
