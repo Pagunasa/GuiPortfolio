9bd46e5282a99a2bdb035fec835642b1544e8cf9
Pagunasa <gui.martinez1996@gmail.com>
2023-12-13 19:38:38 +0100

    Photos and project info

--- js/MyJs.js
+++ js/MyJs.js
@@ -4,3 +4,7 @@ $(document).ready(function() {
   $('#MyNavbar').addClass("navbar-overlap-sticky");
 });
 
+$('#RememberMeowModal').on('hide.bs.modal', function(e) {
+  var $v = $(e.delegateTarget).find('video');
+  $v[0].pause(); 
+});
