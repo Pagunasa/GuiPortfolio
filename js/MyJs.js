2f56779c5ded96a2276f3344e98fa2f4bee7fe04
Pagunasa <gui.martinez1996@gmail.com>
2024-01-01 15:00:34 +0100

    All Projects V1

--- js/MyJs.js
+++ js/MyJs.js
@@ -8,3 +8,18 @@ $('#RememberMeowModal').on('hide.bs.modal', function(e) {
   var $v = $(e.delegateTarget).find('video');
   $v[0].pause(); 
 });
+
+$('#AICustomModal').on('hide.bs.modal', function(e) {
+  var $v = $(e.delegateTarget).find('video');
+  $v[0].pause(); 
+});
+
+$('#CardgeonModal').on('hide.bs.modal', function(e) {
+  var $v = $(e.delegateTarget).find('video');
+  $v[0].pause(); 
+});
+
+$('#NeerasModal').on('hide.bs.modal', function(e) {
+  var $v = $(e.delegateTarget).find('video');
+  $v[0].pause(); 
+});
